eff76c9ff9e4c542b8b02e2b6a59c9c52a16f0dd
Jerome <133158597+Elie00000@users.noreply.github.com>
2025-02-10 10:11:38 +0100

    Update script_after_reboot.sh

--- script_after_reboot.sh
+++ script_after_reboot.sh
@@ -3,38 +3,43 @@
 # Trouver le nom de l'utilisateur non-root
 username=$(logname)
 
-# Passer en root
-sudo su
-
-# Mettre à jour le système
-apt update && apt full-upgrade -y
-
-# Installer les paquets nécessaires
-apt install git build-essential tcl pipx -y
-
-# Cloner le dépôt ail-framework
-git clone https://github.com/ail-project/ail-framework.git /home/$username/ail-framework
-cd /home/$username/ail-framework
-
-# Installer les dépendances
-./installing_deps.sh
-
-# Vérifier si le répertoire AILENV est présent et activer l'environnement virtuel
-if [ -d "/home/$username/ail-framework/AILENV/bin" ]; then
-  . /home/$username/ail-framework/AILENV/bin/activate
-else
-  echo "Le répertoire AILENV n'a pas été trouvé. Veuillez vérifier l'installation."
-  exit 1
-fi
-
-# Naviguer dans le répertoire bin et lancer AIL
-cd /home/$username/ail-framework/bin
-./LAUNCH.sh -l
-sleep 180
-./LAUNCH.sh -k
-
 # Assurer que le chemin de pipx est configuré correctement
 pipx ensurepath
 
-# Redémarrer le système
-reboot
+# Installer Poetry
+pipx install poetry
+pipx ensurepath
+
+# Cloner et préparer valkey
+git clone https://github.com/valkey-io/valkey.git /home/$username/valkey
+cd /home/$username/valkey
+git checkout 7.2
+make
+cd ..
+
+# Cloner et installer lacus
+git clone https://github.com/ail-project/lacus.git /home/$username/lacus
+cd /home/$username/lacus
+poetry install
+poetry shell
+playwright install-deps
+
+# Configurer LACUS_HOME
+echo LACUS_HOME="`pwd`" >> .env
+
+# Copier le fichier de configuration de logging
+cd /home/$username/lacus/config
+cp logging.json.sample logging.json
+
+# Mettre à jour les dépendances avec Poetry
+poetry run update --init
+
+# Installer Tor
+sudo apt install tor -y
+
+# Cloner et configurer pystemon
+cd /home/$username/ail-framework/
+git clone https://github.com/cvandelas/pystemon.git /home/$username/ail-framework/pystemon
+cd /home/$username/ail-framework/pystemon
+rm pystemon.yaml
+cp /home/$username/ail/pystemon.yaml /home/$username/ail-framework/pystemon/
