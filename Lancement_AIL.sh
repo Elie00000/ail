e020220888de4dbd702c3476b9f77db28ed0cc21
Jerome <133158597+Elie00000@users.noreply.github.com>
2025-02-10 13:52:23 +0100

    Update Lancement_AIL.sh

--- Lancement_AIL.sh
+++ Lancement_AIL.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-
+username=$(logname)
 # Ouvrir le premier terminal et exécuter les commandes
 gnome-terminal -- bash -c "
-cd ~/ail-framework/;
-cd bin;
+cd /home/$username/ail-framework/;
+cd /home/$username/ail-framework/bin;
 ./LAUNCH.sh -l;
 ./LAUNCH.sh -f;
-cd  ~/ail-framework/bin/crawlers/lacus;
+cd  /home/$username/ail-framework/bin/crawlers/lacus;
 poetry run start;
-cd ~/ail-framework/pystemon/;
+cd /home/$username/ail-framework/pystemon/;
 ./pystemon.py;
 bash" &
 
 # Ouvrir le deuxième terminal et exécuter les commandes
 gnome-terminal -- bash -c "
 sleep 60
-cd ~/ail-framework/;
-cd bin/importer;
+cd /home/$username/ail-framework/;
+cd /home/$username/ail-framework/bin/importer;
 ./PystemonImporter.py;
 bash"
 
